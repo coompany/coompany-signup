2676a051ecef77d313d28f43880df433f90846a5
Andrea Jemmett <andreajemmett@gmail.com>
2014-06-21 02:20:04 +0200

    html5 mode routing and minor changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
  */
 var app = angular.module('coompanySignup', ['ngRoute', 'ngResource', 'ui.bootstrap']);
 
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'templates/signup.html',
@@ -16,4 +16,7 @@ app.config(function ($routeProvider) {
     .otherwise({
       redirectTo: '/'
     });
+
+  // use the HTML5 History API
+  $locationProvider.html5Mode(true);
 });
